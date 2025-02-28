caa0bf84933e890809cd0223b15061e5cef86746
Matyáš Klimeš <matyasklimes314@seznam.cz>
2025-02-28 21:50:41 +0000

    bugfix 1

--- pages/api/v1/freeplay/[uuid]/revenge.ts
+++ pages/api/v1/freeplay/[uuid]/revenge.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import { z } from "zod"
 import { methodRouter } from "../../../../../lib/methodRouter" // Adjust path accordingly
 import { connectToDatabase } from "../../../../../lib/db"
-import Ranked from "../../../../../models/Ranked"
+import Freeplay from "../../../../../models/FreePlay"
 import { getOnTurn, checkGameEnd } from "../../../../../lib/tictactoe"
 import type { IronSession } from "iron-session"
 import type { NextApiResponseWithSocket, SocketWithData } from "../../../../../pages/api/socket"
@@ -12,70 +12,70 @@ import { createEmptyBoard } from "../../../../../lib/tictactoe"
 const handlePost = async (req: NextApiRequest, res: NextApiResponseWithSocket, data: unknown, session: IronSession<SessionData>) => {
   await connectToDatabase()
 
-  const ranked = await Ranked.findOne({ uuid: req.query.uuid })
-  if (!ranked) {
+  const freeplay = await Freeplay.findOne({ uuid: req.query.uuid })
+  if (!freeplay) {
     return res.status(404).json({
       code: 404,
       message: "Resource not found",
     } as errorMessage)
   }
 
-  if (!ranked.opponentSessionId) {
+  if (!freeplay.opponentSessionId) {
     return res.status(403).json({
       code: 403,
       message: "Game hasnt started yet",
     } as errorMessage)
   }
 
-  if (ranked.creatorSessionId !== session.uuid && ranked.opponentSessionId !== session.uuid) {
+  if (freeplay.creatorSessionId !== session.uuid && freeplay.opponentSessionId !== session.uuid) {
     return res.status(403).json({
       code: 403,
       message: "Forbidden",
     } as errorMessage)
   }
 
-  if (!ranked.finished) {
+  if (!freeplay.finished) {
     return res.status(403).json({
       code: 403,
       message: "Game is not finished",
     } as errorMessage)
   }
 
-  const isCreator = ranked.creatorSessionId === session.uuid
+  const isCreator = freeplay.creatorSessionId === session.uuid
 
   if (isCreator) {
-    ranked.creatorRevenge = true
+    freeplay.creatorRevenge = true
   } else {
-    ranked.opponentRevenge = true
+    freeplay.opponentRevenge = true
   }
 
-  if (ranked.creatorRevenge && ranked.opponentRevenge) {
-    ranked.creatorPoints = 8 * 60
-    ranked.opponentPoints = 8 * 60
-    ranked.startedAt = Date.now()
-    ranked.lastMoveAt = Date.now()
-    ranked.creatorStarts = randomBoolean()
-    ranked.lastMoveX = -1
-    ranked.lastMoveY = -1
-    ranked.board = createEmptyBoard()
-    ranked.creatorRevenge = false
-    ranked.opponentRevenge = false
-    ranked.finished = false
+  if (freeplay.creatorRevenge && freeplay.opponentRevenge) {
+    freeplay.creatorPoints = 8 * 60
+    freeplay.opponentPoints = 8 * 60
+    freeplay.startedAt = Date.now()
+    freeplay.lastMoveAt = Date.now()
+    freeplay.creatorStarts = randomBoolean()
+    freeplay.lastMoveX = -1
+    freeplay.lastMoveY = -1
+    freeplay.board = createEmptyBoard()
+    freeplay.creatorRevenge = false
+    freeplay.opponentRevenge = false
+    freeplay.finished = false
   }
 
   try {
-    await ranked.save()
+    await freeplay.save()
 
     const io = res.socket.server.io
     if (io) {
       io.sockets.sockets.forEach((socket: SocketWithData) => {
-        if (socket.sessionUUID === ranked.creatorSessionId || socket.sessionUUID === ranked.opponentSessionId) {
-          socket.emit(`rankedUpdate:${ranked.uuid}`, ranked)
+        if (socket.sessionUUID === freeplay.creatorSessionId || socket.sessionUUID === freeplay.opponentSessionId) {
+          socket.emit(`freeplayUpdate:${freeplay.uuid}`, freeplay)
         }
       })
     }
 
-    res.status(200).json(ranked)
+    res.status(200).json(freeplay)
   } catch (error: any) {
     res.status(422).json({ code: 422, message: error.message } as errorMessage)
   }
